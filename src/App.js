6267ff775cf2e63e7a56bf63a3d91372be171b0d
DopeCoder24 <ddmondal14@gmail.com>
2024-11-07 20:12:28 +0530

    commented productpage and scorecaed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Homepage from "./pages/Homepage";
-import Productspage from "./pages/Productspage";
-import Scorecard from "./components/Scorecard";
+// import Productspage from "./pages/Productspage";
+// import Scorecard from "./components/Scorecard";
 // import { Switch, Route } from "react-router-dom";
 // i have to fetch data from api
 function App() {
